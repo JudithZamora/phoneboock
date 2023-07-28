f17965aa7efc126a549ec9622544063446b8526a
King Nodens <judith.zamora.go@gmail.com>
2023-07-27 18:25:07 -0600

    agregue carpeta src

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const ContactForm = ({ addContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [state, setState] = useState({name: '', number: ''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addContact(state);
+    
+    setName('');
+    setNumber('');
+    
+  };
+
+  const handleChange = e => {
+  setState ((prevState) => ({...prevState, [e.target.name]: e.target.value}));
+  };
+  console.log(state);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>NAME</h2>
+      <input
+        type="text"
+        name="name"
+        value={state.name}
+        onChange={handleChange}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name"
+        required
+      />
+      <h2>NUMBER</h2>
+      <input
+        type="tel"
+        name="number"
+        value={state.number}
+        onChange={handleChange}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+ContactForm.propTypes = {
+  addContact: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
